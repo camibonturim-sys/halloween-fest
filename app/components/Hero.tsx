9c62065802e4addba9ec999b94c85c7e3b293182
camibonturim-sys <camibonturim@gmail.com>
2026-08-21 19:49:08 +0000

    Ajusta capa para celular

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,14 +1,14 @@
 export default function Hero() {
   return (
     <section
-      className="relative min-h-[85vh] bg-black bg-cover bg-center bg-no-repeat"
+    className="relative min-h-[56vw] bg-black bg-contain bg-top bg-no-repeat md:min-h-[85vh] md:bg-cover md:bg-center"
       style={{
         backgroundImage: "url('/capa-halloween.png')",
       }}
     >
       <div className="absolute inset-0 bg-black/10" />
 
-      <div className="relative z-10 flex min-h-[85vh] items-end justify-center px-6 pb-10 md:justify-start md:px-24 md:pb-20">
+      <div className="relative z-10 flex min-h-[56vw] md:min-h-[85vh]npm run build items-end justify-center px-6 pb-10 md:justify-start md:px-24 md:pb-20">
         <a
           href="#ingressos"
           className="rounded-xl bg-orange-500 px-8 py-4 text-lg font-black uppercase text-black shadow-[0_0_30px_rgba(249,115,22,0.45)] transition hover:scale-105 hover:bg-orange-400"
